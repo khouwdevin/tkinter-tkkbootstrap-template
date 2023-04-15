4f4221e671cc3400699bc2bd37b2043c35fa15a1
khouwdevin <khouwdevin@gmail.com>
2023-04-15 16:35:44 +0700

    change back to pack

--- src/pages/mainpage.py
+++ src/pages/mainpage.py
@@ -6,7 +6,7 @@ class MainPage(ttk.Frame):
         super().__init__(parent)
 
         label = ttk.Label(self, text ="Main Page", font = "TimesNewRoman 18 bold")
-        label.place(anchor = "center", relx = 0.5, rely = 0.2)
+        label.pack()
 
         button = ttk.Button(self, text = "Change Page", bootstyle=(INFO, OUTLINE), command = lambda:controller.change_page(1))
-        button.place(anchor = "center", relx = 0.5, rely = 0.5)
+        button.pack()
