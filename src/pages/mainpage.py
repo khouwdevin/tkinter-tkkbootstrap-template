9c0bcd242c333eae19850b316d59c71fbfaaa4b2
khouwdevin <khouwdevin@gmail.com>
2023-04-15 16:37:02 +0700

    add padding

--- src/pages/mainpage.py
+++ src/pages/mainpage.py
@@ -6,7 +6,7 @@ class MainPage(ttk.Frame):
         super().__init__(parent)
 
         label = ttk.Label(self, text ="Main Page", font = "TimesNewRoman 18 bold")
-        label.pack()
+        label.pack(pady = 10)
 
         button = ttk.Button(self, text = "Change Page", bootstyle=(INFO, OUTLINE), command = lambda:controller.change_page(1))
-        button.pack()
+        button.pack(pady = 10)
--- src/pages/secondpage.py
+++ src/pages/secondpage.py
@@ -6,7 +6,7 @@ class SecondPage(ttk.Frame):
         super().__init__(parent)
 
         label = ttk.Label(self, text ="Second Page", font = "TimesNewRoman 18 bold")
-        label.pack()
+        label.pack(pady = 10)
 
         button = ttk.Button(self, text = "Change Page", bootstyle=(INFO, OUTLINE), command = lambda:controller.change_page(0))
-        button.pack()
+        button.pack(pady = 10)
