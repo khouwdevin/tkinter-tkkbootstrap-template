f4b8838e1ca87ac519ac8168f9f3c8d3dc4ed6a6
khouwdevin <khouwdevin@gmail.com>
2023-04-15 16:31:20 +0700

    fix frame bug

--- src/pages/mainpage.py
+++ src/pages/mainpage.py
@@ -5,8 +5,8 @@ class MainPage(ttk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
 
-        label = ttk.Label(text ="Main Page", font = "TimesNewRoman 18 bold")
-        label.pack(pady = 10)
+        label = ttk.Label(self, text ="Main Page", font = "TimesNewRoman 18 bold")
+        label.grid(row = 0, column = 0, pady = 10)
 
-        self.button = ttk.Button(text = "Change Page", bootstyle=(INFO, OUTLINE), command = lambda:controller.change_page(1))
-        self.button.pack(pady = 10)
+        button = ttk.Button(self, text = "Change Page", bootstyle=(INFO, OUTLINE), command = lambda:controller.change_page(1))
+        button.grid(row = 1, column = 0, pady = 10)
